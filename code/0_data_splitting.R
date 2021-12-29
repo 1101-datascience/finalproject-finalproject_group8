45f1e7fb244da5be0a8d16cfe49542d4bd092c10
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2021-12-29 11:21:55 +0800

    Update data split

--- code/0_data_splitting.R
+++ code/0_data_splitting.R
@@ -1,5 +1,7 @@
 df <- read.csv('./data/creditcard.csv')
 
+
+# 訓練與測試資料切割 ===========================================
 # 這次資料為時間序列，會按照時間順序去切，不隨機
 # df <- df[sample(nrow(df)), ] #sample rows 
 
@@ -7,13 +9,9 @@ df <- read.csv('./data/creditcard.csv')
 bound <- floor((nrow(df)/10)*8) #define % of training and test set
 
 
-df.train <- df[1:bound, ] 
-# Time: 0 ~ 145247 -> 40 小時 20 分鐘
-# Class 比例 -> 0.00183
+df.train <- df[1:bound, ] # Time: 0 ~ 145247 -> 40 小時 20 分鐘 | Class 比例 -> 0.00183
 
-df.test <- df[(bound+1):nrow(df), ] 
-# Time: 145248 ~ 172792 -> 7 小時 40 分鐘
-# Class 比例 -> 0.001317
+df.test <- df[(bound+1):nrow(df), ] # Time: 145248 ~ 172792 -> 7 小時 40 分鐘 | Class 比例 -> 0.001317
 
 ## 切法二:造時間切 ##
 df.train <- df[which(df$Time < 144000), ] # 40小時 | Class 比例: 0.001846 
