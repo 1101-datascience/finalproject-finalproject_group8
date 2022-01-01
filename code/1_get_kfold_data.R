fd23829d568e922a2343b7afc45e582ad7f8276a
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-01-01 19:38:09 +0800

    Update 1_get_kfold_data.R

--- code/1_get_kfold_data.R
+++ code/1_get_kfold_data.R
@@ -1,3 +1,16 @@
+source('utils.R')
+
+# 決策樹模型
+check_library('rpart')
+library(rpart)
+
+# 視覺化
+check_library('ggplot2')
+library(ggplot2)
+
+# 資料處理
+check_library('tidyr')
+library(tidyr)
 # function >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # 先各個標籤分開抽樣處理，最後將各組併在一起(除不盡的隨機配到各組)
 get_kfold_idx <- function (d, k, label_col) {
@@ -69,7 +82,150 @@ get_train_val_fold <- function (test_fold, max_fold) {
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 # main process >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+label <- 'Class'
+test_fold_ls <- c(4, 5, 6, 7, 8, 9, 10)
+train <- read.csv('./data/train.csv')
+
+result_path <- './output/1_kfold/result.csv'
+result_select_path <- './output/1_kfold/result_selct.csv'
+pred_path <- './output/1_kfold/pred/'
+kfoldid_path <- './output/1_kfold/kfold_idx.rds'
+
+build_folder(result_path)
+build_folder(result_select_path)
+build_folder(pred_path, isfile=FALSE)
+build_folder(kfoldid_path)
+
+# train 
+fold_result <- list()
+for (test_fold in test_fold_ls) {
+  fold_result[[test_fold]] <- list()
+}
+
+for (test_fold in test_fold_ls) {
+  kfold_idx <- get_kfold_idx(train, test_fold, label) # 將 id 分成 fold 份
+  
+  for (k in 1:test_fold) {
+    start_time <- Sys.time()
+    print(paste(test_fold, '-', k))
+    # 以k決定 train、val、test 在這個 fold 分到的 idx
+    split_fold <- get_train_val_fold(k, test_fold)
+    
+    train_fold_idx <- c()
+    for (i in kfold_idx[split_fold$train]) {
+      train_fold_idx <- c(train_fold_idx, i)
+    }
+    
+    # 不用到 val 資料，所以將資料和到 train
+    val_fold_idx <- kfold_idx[[split_fold$val]]
+    test_fold_idx <- c(val_fold_idx, kfold_idx[[split_fold$test]])
+    
+    # 用 idx 選取 train、val、test 資料
+    train_f <- train[train_fold_idx,]
+    test_f <- train[test_fold_idx, ]
+    
+    train_f[, label] <- as.factor(train_f[, label])
+    test_f[, label] <- as.factor(test_f[, label])
+    
+    DT.model <- rpart(formula(paste(label, '~', '.')),
+                      data=train_f, control=rpart.control(maxdepth=4),
+                      method="class")
+    fold_result[[test_fold]][[k]] <- get_evaluate(DT.model, train_f, test_f, label)
+    
+    fold_result[[test_fold]][[k]][['timeuse']] <- Sys.time() - start_time
+  }
+}
+
+# sort result
+result <- data.frame()
+for (test_fold in test_fold_ls) {
+  for (k in 1:test_fold) {
+    print(paste(test_fold, '-', k))
+    k_result <- list(test_fold=test_fold, fold=k)
+    k_result <- c(k_result,
+                  fold_result[[test_fold]][[k]]$result
+    )
+    k_result <- c(k_result,
+      list(timeuse=fold_result[[test_fold]][[k]]$timeuse[[1]])
+    )
+    result <- rbind(result, data.frame(k_result))
+    
+    pred_tables <- fold_result[[test_fold]][[k]]$table
+    for (pred_table in names(pred_tables)) {
+      write.csv(pred_tables[[pred_table]], 
+                paste0(pred_path, 'test', test_fold, '_fold', k, '_', pred_table, '.csv'), 
+                row.names = FALSE, quote = FALSE)
+    }
+  }
+}
+
+write.csv(result, result_path, 
+          row.names = FALSE, quote = FALSE)
 
 
+# Visualization of results
+result_select <- data.frame()
+for (test_fold in test_fold_ls) {
+  test_fold_result <- result[result$test_fold == test_fold, ]
+  test_fold_result$fold <- NULL
+  
+  test_fold_result_ls <-list(test_fold=test_fold)
+  for (col in names(test_fold_result)) {
+    if (col != 'test_fold') {
+      test_fold_result_ls[[paste0(col, '_ave')]]  <- mean(test_fold_result[[col]])
+      test_fold_result_ls[[paste0(col, '_var')]]  <- var(test_fold_result[[col]])
+    }
+  }
+  result_select <- rbind(result_select, data.frame(test_fold_result_ls))
+}
 
-# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+write.csv(result_select, result_select_path, 
+          row.names = FALSE, quote = FALSE)
+
+# ggplot(result_select, aes(x=test_fold)) + 
+#   geom_line(aes(y = test_ave), color = "darkred", size=2) +
+#   geom_line(aes(y = test_precision_ave), color="steelblue", size=2) +
+#   geom_line(aes(y = test_recall_ave), color="turquoise", size=2) +
+#   geom_line(aes(y = test_f1_ave), color="tan1", size=2) +
+#   theme_bw()
+
+# 平均
+aggNew <- result_select %>% pivot_longer(cols =  c('test_ave',
+                                                   'test_precision_ave',
+                                                   'test_recall_ave',
+                                                   'test_f1_ave'
+                                                   ),
+                                    names_to = "Evaluation", 
+                                    values_to = "Value")
+
+COLORS <- c(test_ave = "darkred", test_precision_ave ="steelblue",  
+            test_recall_ave = "turquoise" , test_f1_ave = "tan1")
+ggplot(aggNew, aes(x = test_fold, y = Value, group = Evaluation, color = Evaluation)) +
+  geom_line(size = 0.9) +
+  scale_color_manual(values = COLORS)
+
+# 變異
+aggNew <- result_select %>% pivot_longer(cols =  c('test_var',
+                                                   'test_precision_var',
+                                                   'test_recall_var',
+                                                   'test_f1_var'
+                                                   ),
+                                         names_to = "Evaluation", 
+                                         values_to = "Value")
+
+COLORS <- c(test_var = "darkred", test_precision_var ="steelblue",  
+            test_recall_var = "turquoise" , test_f1_var = "tan1")
+ggplot(aggNew, aes(x = test_fold, y = Value, group = Evaluation, color = Evaluation)) +
+  geom_line(size = 0.9) +
+  scale_color_manual(values = COLORS)
+
+
+# 時間
+result_select$timeuse_total <- result_select$timeuse_ave* result_select$test_fold
+ggplot(result_select, aes(x = test_fold, y = timeuse_total)) +
+  geom_line(size = 0.9, color="darkred")
+# output/fold_idx
+
+kfold_idx <- get_kfold_idx(train, 4, label) # 將 id 分成 fold 份
+
+saveRDS(kfold_idx, kfoldid_path)
