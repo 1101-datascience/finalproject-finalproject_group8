088956bc1b32f3714e4527a28ef4b5647e4d3913
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-01-01 21:56:37 +0800

    Create 2_baseline.R

---
+++ code/2_baseline.R
@@ -0,0 +1,140 @@
+source('utils.R')
+
+# 決策樹模型
+check_library('rpart')
+library(rpart)
+
+# 資料準備 =================================================
+fold <- 4
+label <- 'Class'
+kfoldid_path <- './output/1_kfold/kfold_idx.rds'
+train_path <- "./data/train.csv"
+test_path <- "./data/test.csv"
+
+result_path <- './output/2_baseline/result.csv'
+result_select_path <- './output/2_baseline/result_selct.csv'
+pred_path <- './output/2_baseline/pred/'
+
+build_folder(result_path)
+build_folder(result_select_path)
+build_folder(pred_path, isfile=FALSE)
+
+kfold_idx <- readRDS(kfoldid_path)
+
+train <- read.csv(train_path)
+test <- read.csv(test_path)
+
+train.f <- train
+train.f$Class <- NULL
+train.label <- train$Class
+
+# 實驗 1 ==============================================
+experiment_ls <- list(
+  'decision trees(depth=2)' = function(train, label){
+    model <- rpart(formula(paste(label, '~', '.')),
+                      data=train, control=rpart.control(maxdepth=2),
+                      method="class")
+  },
+  'decision trees(depth=5)' = function(train, label){
+    model <- rpart(formula(paste(label, '~', '.')),
+                   data=train, control=rpart.control(maxdepth=5),
+                   method="class")
+  },  
+  'decision trees(depth=10)' = function(train, label){
+    model <- rpart(formula(paste(label, '~', '.')),
+                   data=train, control=rpart.control(maxdepth=10),
+                   method="class")
+  },
+  'decision trees(depth=20)' = function(train, label){
+    model <- rpart(formula(paste(label, '~', '.')),
+                   data=train, control=rpart.control(maxdepth=20),
+                   method="class")
+  },
+  'decision trees(depth=30)' = function(train, label){
+    model <- rpart(formula(paste(label, '~', '.')),
+                   data=train, control=rpart.control(maxdepth=30),
+                   method="class")
+  }
+)
+
+fold_result <- list()
+for (experiment in names(experiment_ls)) {
+  fold_result[[experiment]] <- list()
+  for (k in 1:fold) {
+    print(paste(experiment, '-', k))
+    start_time <- Sys.time()
+    split_fold <- get_train_val_fold(k, fold)
+    
+    train_fold_idx <- c()
+    for (i in kfold_idx[split_fold$train]) {
+      train_fold_idx <- c(train_fold_idx, i)
+    }
+    
+    # 不用到 test，所以將資料和到 train
+    train_fold_idx <- c(train_fold_idx, kfold_idx[[split_fold$test]])
+    
+    val_fold_idx <- kfold_idx[[split_fold$val]]
+    
+    # 用 idx 選取 train、val、test 資料
+    train_f <- train[train_fold_idx,]
+    val_f <- train[val_fold_idx, ]
+    test_f <- test # 固定的 test 資料集
+    
+    train_f[, label] <- as.factor(train_f[, label])
+    val_f[, label] <- as.factor(val_f[, label])
+    test_f[, label] <- as.factor(test_f[, label])
+    
+    model <- experiment_ls[[experiment]](train_f, label)
+    fold_result[[experiment]][[k]] <- get_evaluate(model, 
+                                                  train = train_f, 
+                                                  test = test_f, 
+                                                  val = val_f,
+                                                  label = label)
+    
+    fold_result[[experiment]][[k]][['timeuse']] <- Sys.time() - start_time
+  }
+}
+
+result <- data.frame()
+for (experiment in names(experiment_ls)) {
+  for (k in 1:fold) {
+    print(paste(experiment, '-', k))
+    k_result <- list(experiment=experiment, fold=k)
+    k_result <- c(k_result,
+                  fold_result[[experiment]][[k]]$result
+    )
+    k_result <- c(k_result,
+                  list(timeuse=fold_result[[experiment]][[k]]$timeuse[[1]])
+    )
+    result <- rbind(result, data.frame(k_result))
+    
+    pred_tables <- fold_result[[experiment]][[k]]$table
+    for (pred_table in names(pred_tables)) {
+      write.csv(pred_tables[[pred_table]], 
+                paste0(pred_path, experiment, '_fold', k, '_', pred_table, '.csv'), 
+                row.names = FALSE, quote = FALSE)
+    }
+  }
+}
+
+write.csv(result, result_path, 
+          row.names = FALSE, quote = FALSE)
+
+result_select <- data.frame()
+for (experiment in names(experiment_ls)) {
+  test_fold_result <- result[result$experiment == experiment, ]
+  test_fold_result$fold <- NULL
+  
+  test_fold_result_ls <-list(experiment=experiment)
+  for (col in names(test_fold_result)) {
+    if (col != 'test_fold') {
+      test_fold_result_ls[[paste0(col, '_ave')]]  <- mean(test_fold_result[[col]])
+      test_fold_result_ls[[paste0(col, '_var')]]  <- var(test_fold_result[[col]])
+    }
+  }
+  result_select <- rbind(result_select, data.frame(test_fold_result_ls))
+}
+
+write.csv(result_select, result_select_path, 
+          row.names = FALSE, quote = FALSE)
+# 實驗 2 ==============================================
